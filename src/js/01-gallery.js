0d343174ed3472c0ae79854031f42594440997f6
Olena Bondar <olena.bondar1060@gmail.com>
2022-11-04 22:26:01 +0200

    update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryRef = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const markupGallery = createMarkupGallery(galleryItems);
+
+function createMarkupGallery(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>`
+    )
+    .join('');
+}
+
+galleryRef.insertAdjacentHTML('beforeend', markupGallery);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+console.log(gallery);
