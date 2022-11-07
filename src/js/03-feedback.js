143667d57c572818d05455daa182b534a554085b
Olena Bondar <olena.bondar1060@gmail.com>
2022-11-07 12:52:48 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('form'),
+  input: document.querySelector('input'),
+  textArea: document.querySelector('textarea'),
+};
+
+refs.form.addEventListener('input', throttle(onSaveValue, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+const FEEDBACK_KEY = 'feedback-form-state';
+const formData = {};
+let saveFormData = '';
+let storageData = '';
+let getStorageData = {};
+
+onInputSaveValue();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  console.log(formData);
+  e.currentTarget.reset();
+  localStorage.removeItem(FEEDBACK_KEY);
+}
+
+function onSaveValue(e) {
+  formData[e.target.name] = e.target.value;
+
+  saveFormData = JSON.stringify(formData);
+  localStorage.setItem(FEEDBACK_KEY, saveFormData);
+}
+
+function onInputSaveValue() {
+  storageData = localStorage.getItem(FEEDBACK_KEY);
+  getStorageData = JSON.parse(storageData);
+
+  if (storageData) {
+    refs.input.value = getStorageData.email;
+    refs.textArea.value = getStorageData.message;
+  }
+}
